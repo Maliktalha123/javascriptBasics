afde235173589869737e7311ddfbf9145bd35f1c
Maliktalha123 <talhanoormalik0321@gmail.com>
2024-02-23 09:24:30 +0500

    it is for assigment sbmission of SMIT

---
+++ app.js
@@ -0,0 +1,35 @@
+var userName = prompt("Enter Your Name");
+alert("Hello ! Mr." +userName +" "+"Welcome to Our Page");
+
+
+var firstNum = +prompt("Enter First Number");
+var operator = prompt("Enter the operator sign");
+var secondnum = +prompt("Enter the Second Num");
+
+if (operator == "+") {
+    alert(firstNum + secondnum)
+}
+ else if (operator == "-"){
+    alert(firstNum - secondnum)
+} else if (operator == "*"){
+    alert(firstNum * secondnum)
+} else if (operator == "/"){
+    alert(firstNum / secondnum)
+}
+
+
+
+var firstName = prompt("Enter Your First Name");
+var second_name = prompt("Enter Your Second Name");
+alert ( firstName + " "+second_name);
+
+
+var number = prompt("Enter Your Number");
+var soluting = number % 2;
+
+if(soluting==0){
+    alert("Your Number is Even Number")
+}
+else{
+    alert("YOur Number is ODD Number");
+}
